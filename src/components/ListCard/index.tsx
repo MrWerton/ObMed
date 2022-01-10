aba2f02435705bd061193f9d7ac65449df64ecb8
MrWerton <mr.werton@gmail.com>
2022-01-10 17:00:11 -0300

    created the list card

---
+++ src/components/ListCard/index.tsx
@@ -0,0 +1,49 @@
+import { Card } from "../Card"
+import './styles.css'
+
+import OpenIcon from '../../assets/images/open.svg'
+import HeartIcon from '../../assets/images/Heart.svg'
+import ExamIcon from '../../assets/images/Quimic.svg'
+import FinanceIcon from '../../assets/images/Money.svg'
+import SthetoscopeIcon from '../../assets/images/Sthetoscope.svg'
+import BandAidIcon from '../../assets/images/BandAid.svg'
+import { ButtonFloat } from "../ButtonFloat"
+
+const InfoCard = [
+    {
+        title: "Consultas",
+        descriptionImg: "icon of stethoscope",
+        img: SthetoscopeIcon
+    },
+    {
+        title: "Exames Elaboratoriais",
+        descriptionImg: "icon of quimic",
+        img: ExamIcon
+    },
+    {
+        title: "Procedimentos",
+        descriptionImg: "icon of stethoscope",
+        img: BandAidIcon
+    },
+    {
+        title: "Exames Elaboratoriais",
+        descriptionImg: "icon of stethoscope",
+        img: HeartIcon
+    },
+    {
+        title: "Consultas",
+        descriptionImg: "Financiamento",
+        img: FinanceIcon
+    },
+]
+export const ListCard: React.FC = ()=>{
+    return(
+        <div className="Container-Card">
+           {InfoCard.map(card=>(
+               <Card img={card.img} descriptionImg={card.descriptionImg} title={card.title}/>
+
+           ))}
+           <ButtonFloat/>
+        </div>
+    )
+}
