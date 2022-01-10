5d4c30b99bffc5f356922befa4f6d8e538ad5aa9
MrWerton <mr.werton@gmail.com>
2022-01-10 17:01:06 -0300

    created the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {  IonPage } from '@ionic/react';
+import { Header } from '../components/Header';
+import { ListCard } from '../components/ListCard';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+    <IonPage className="Home">
+      <Header/>
+      <ListCard/>
+      
     </IonPage>
   );
 };
